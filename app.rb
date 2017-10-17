19c9e65c74f90de080cfd4cf716346db62aa7c52
Elise Barnes <barnese3@go.stockton.edu>
2017-10-17 17:59:39 -0400

    updated home and profile

--- app.rb
+++ app.rb
@@ -4,12 +4,29 @@ require 'sinatra/flash'
 require 'sqlite3'
 require './models'
 
+enable :sessions
 set :database, {adapter: 'sqlite3', database: 'microblog.sqlite3'}
+
+before do
+  current_user
+end
 
 get '/' do
 	@posts = BlogPost.all
 	@class = "home"
 	erb :home
+end
+
+post '/' do
+	user = User.find_by(username: params[:username])
+	if user && user.password == params[:password]
+		session[:user_id] = user.id
+		flash[:message] = "You're now logged in!"
+		redirect '/'
+	else
+		flash[:message] = "Email and password did not match"
+		redirect '/'
+	end
 end
 
 get '/profile' do
@@ -32,4 +49,14 @@ end
 
 get '/writeblogpost' do
   erb :writeblogpost
+end
+
+get '/logout' do
+  session[:user_id] = nil
+  flash[:message] = "You're logged out. Cool!"
+  redirect '/'
+end
+
+def current_user
+  @current_user = User.find(session[:user_id]) if session[:user_id]
 end
