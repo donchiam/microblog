0377c94f17f4d802cd0040d34083a8a3fe9120d7
Don Chiamthamachinda <don.chiam@gmail.com>
2017-10-17 16:29:16 -0400

    updated app.rb

--- app.rb
+++ app.rb
@@ -8,24 +8,15 @@ set :database, {adapter: 'sqlite3', database: 'microblog.sqlite3'}
 
 get '/' do
 	@posts = BlogPost.all
-<<<<<<< HEAD
 	@users = User.all
 	@class = "home"
   	erb :home
-=======
-	@class = "home"
-	erb :home
-end
 
-get '/profile' do
-	@class = "profile"
-	erb :profile
 end
 
 get '/blogpost/:id' do
 	@blogpost = BlogPost.find(params[:id])
 	erb :viewblogpost
->>>>>>> 7cba97ac5a024302e14e9e79b7f108b59ebe8e6b
 end
 
 get '/signup' do
@@ -36,7 +27,6 @@ get '/login' do
   	erb :login
 end
 
-<<<<<<< HEAD
 get '/profile/:id' do
 	@user = User.find(params[:id])
   	erb :profile
@@ -45,8 +35,5 @@ end
 get '/writeblogpost' do
   	erb :writeblogpost
 end
-=======
-get '/writeblogpost' do
-  erb :writeblogpost
-end
->>>>>>> 7cba97ac5a024302e14e9e79b7f108b59ebe8e6b
+
+
