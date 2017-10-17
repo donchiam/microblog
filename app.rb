8949b4f215ffb210017ca78184a762b45eef9ce8
Don Chiamthamachinda <don.chiam@gmail.com>
2017-10-17 16:03:46 -0400

    updated lots of files

--- app.rb
+++ app.rb
@@ -5,3 +5,24 @@ require 'sqlite3'
 require './models'
 
 set :database, {adapter: 'sqlite3', database: 'microblog.sqlite3'}
+
+
+get '/' do
+  erb :home
+end
+
+get '/signup' do
+  erb :signup
+end
+
+get '/login' do
+  erb :login
+end
+
+get '/profile' do
+  erb :profile
+end
+
+get '/writeblogpost' do
+  erb :writeblogpost
+end
