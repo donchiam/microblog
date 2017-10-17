e4b0a170aa64532555588b25d25dbeb25fcb08a3
Don Chiamthamachinda <don.chiam@gmail.com>
2017-10-17 16:25:25 -0400

    update app.rb

--- app.rb
+++ app.rb
@@ -8,21 +8,25 @@ set :database, {adapter: 'sqlite3', database: 'microblog.sqlite3'}
 
 
 get '/' do
-  erb :home
+	@posts = BlogPost.all
+	@users = User.all
+	@class = "home"
+  	erb :home
 end
 
 get '/signup' do
-  erb :signup
+  	erb :signup
 end
 
 get '/login' do
-  erb :login
+  	erb :login
 end
 
-get '/profile' do
-  erb :profile
+get '/profile/:id' do
+	@user = User.find(params[:id])
+  	erb :profile
 end
 
 get '/writeblogpost' do
-  erb :writeblogpost
+  	erb :writeblogpost
 end
